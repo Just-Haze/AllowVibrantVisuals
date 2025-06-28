3b79b3542c49519fad9b87e8536799080a34271e
Oryxel <gamingnoob681@gmail.com>
2025-06-28 17:36:11 +0700

    Working.

---
+++ src/main/java/org/oryxel/novibrantvisuals/NoVibrantVisuals.java
@@ -0,0 +1,12 @@
+package org.oryxel.novibrantvisuals;
+
+import org.geysermc.event.subscribe.Subscribe;
+import org.geysermc.geyser.api.event.bedrock.SessionLoadResourcePacksEvent;
+import org.geysermc.geyser.api.extension.Extension;
+
+public class NoVibrantVisuals implements Extension {
+    @Subscribe
+    public void onSessionLoadResourcePacks(SessionLoadResourcePacksEvent event) {
+        event.allowVibrantVisuals(false);
+    }
+}
